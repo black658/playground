8f93617f0c519fd491392e945ddc4c83a3112eb0
Chaochao Zhan <chaochao.zhan@outlook.com>
2024-03-17 00:38:22 -0400

    added controller

--- src/main/groovy/com/chaochao/playground/PlaygroundApplication.groovy
+++ src/main/groovy/com/chaochao/playground/PlaygroundApplication.groovy
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class PlaygroundApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(PlaygroundApplication, args)
-	}
+    static void main(String[] args) {
+        SpringApplication.run(PlaygroundApplication, args)
+    }
 
 }
---
+++ src/main/groovy/com/chaochao/playground/controller/Controller.groovy
@@ -0,0 +1,15 @@
+package com.chaochao.playground.Controller
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+
+
+@RestController
+class Controller {
+
+    @GetMapping
+    String get() {
+        "f2fd1asfg"
+    }
+
+}
